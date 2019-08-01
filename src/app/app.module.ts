bb6bdb1620a14fff0738c7d3afe05598f364dd82
xiaohuiguo <1836250418@qq.com>
2019-08-02 00:05:42 +0800

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,7 +22,7 @@ import { DataService } from './data-center/services/data.service';
     GuangzhouDatacenterComponent
   ],
   imports: [
-    BrowserModule.withServerTransition({appId: 'ng-universal-demo'}),
+    BrowserModule.withServerTransition({appId: 'ng-universal-demo'}), // 打包时的id，只要保证和其它配置不重复即可。
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full'},
       { path: 'lazy', loadChildren: './lazy/lazy.module#LazyModule'},
@@ -31,7 +31,7 @@ import { DataService } from './data-center/services/data.service';
       // { path: 'dataCenter/:region/:address', component: DcComponent},
       { path: 'dataCenter/huanan/guangzhou', component: GuangzhouDatacenterComponent}
     ]),
-    TransferHttpCacheModule,
+    TransferHttpCacheModule, // 用于实现服务器到客户端的请求传输缓存，防止客户端重复请求服务端已完成的请求
     // NgUniversalModule
   ],
   providers: [CommonService, SsrService,DataService],
